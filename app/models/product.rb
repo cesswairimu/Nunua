f48f9afa0765b2b2f6dad45f47251aeeb18ea2b5
Cess <cessmbuguar@gmail.com>
2019-08-27 21:41:48 +0300

    validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  validates_presence_of :title, :image_url, :description, :price
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with:
+    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
